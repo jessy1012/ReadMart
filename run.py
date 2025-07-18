07f5ade6d4528b747057b35d2422d1460478e4fd
因为西经被唐僧取了呀 <1282785671@qq.com>
2025-07-18 15:56:57 +0800

    project commit

---
+++ run.py
@@ -0,0 +1,23 @@
+import shutil
+import pytest
+import os
+import webbrowser
+from conf.setting import REPORT_TYPE
+import sys
+
+if __name__ == '__main__':
+
+
+    print(f"(sys.argv): {sys.argv}")
+
+    if REPORT_TYPE == 'allure':
+        pytest.main(
+            ['-s', '-v', '--alluredir=./report/temp', './testcase', '--clean-alluredir',
+             '--junitxml=./report/results.xml'])
+
+        shutil.copy('./environment.xml', './report/temp')
+        os.system(f'allure serve ./report/temp')
+
+    elif REPORT_TYPE == 'tm':
+        pytest.main(['-vs', '--pytest-tmreport-name=testReport.html', '--pytest-tmreport-path=./report/tmreport'])
+        webbrowser.open_new_tab(os.getcwd() + '/report/tmreport/testReport.html')
