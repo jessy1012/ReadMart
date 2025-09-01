71210869deba364578a7336e9736096af24af37f
因为西经被唐僧取了呀 <1282785671@qq.com>
2025-09-01 20:27:19 +0800

    去掉--clean-alluredir

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     if REPORT_TYPE == 'allure':
         pytest.main(
-            ['-s', '-v', '--alluredir=./report/temp', './testcase', '--clean-alluredir',
+            ['-s', '-v', '--alluredir=./report/temp', './testcase',
              '--junitxml=./report/results.xml'])
 
         shutil.copy('./environment.xml', './report/temp')
